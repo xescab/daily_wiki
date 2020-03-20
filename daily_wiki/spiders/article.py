fdb003477eb0f5e1ef4728cdcc91d257c6da191e
Xesc Arbona <xesc.arbona@gmail.com>
2020-03-20 18:17:36 +0100

    Export to JSON

--- daily_wiki/spiders/article.py
+++ daily_wiki/spiders/article.py
@@ -8,6 +8,12 @@ class ArticleSpider(scrapy.Spider):
     allowed_domains = ['en.wikipedia.org']
     start_urls = ['https://en.wikipedia.org/wiki/Wikipedia:Featured_articles']
 
+    # Enable Feed Storage
+    custom_settings = {
+        'FEED_FORMAT': 'json',
+        'FEED_URI': 'file:///tmp/featured-articles-%(time)s.json'
+    }
+
     def parse(self, response):
         host = self.allowed_domains[0]
         for link in response.css(".featured_article_metadata > a"):
